727a2b89312bfc45a91e4bf79129258efd0e7072
jo314652 <jo314652@gmail.com>
2017-10-02 21:36:42 +0800

    st

---
+++ source/Main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+	printf("    CCC   \n");
+	printf("  C     C \n");
+	printf(" C       C\n");
+	printf(" C       C\n");
+	printf("  C     C \n");
+	printf("          \n");
+	printf("    CCC   \n");
+	printf("  C     C \n");
+	printf(" C       C\n");
+	printf(" C       C\n");
+	printf("  C     C \n");
+	printf("          \n");
+	printf(" HHHHHHHHH\n");
+	printf("     H    \n");
+	printf("     H    \n");
+	printf("     H    \n");
+	printf(" HHHHHHHHH\n");
+
+
+	system("pause");
+	return 0;
+}
